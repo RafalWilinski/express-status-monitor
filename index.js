a541985e1eb775438784779dc5d5c71100e222ae
Rafal Wilinski <raf.wilinski@gmail.com>
2016-08-07 20:39:15 +0200

    📫 Add support for Response Times

--- index.js
+++ index.js
@@ -4,33 +4,35 @@
   const path = require('path');
   const onHeaders = require('on-headers');
   const pidusage = require('pidusage');
-  const responseTimes = [];
-  const osStats = [];
 
   const defaultConfig = {
     socketPort: 41338,
     path: '/status',
-    interval: 1,
-    retention: 100,
+    spans: [{
+      interval: 1,
+      retention: 60
+    }]
   };
 
-  const gatherOsMetrics = (io, config) => {
+  const gatherOsMetrics = (io, span) => {
     pidusage.stat(process.pid, (err, stat) => {
       stat.timestamp = Date.now();
+
       // Convert from B to MB
       stat.memory = stat.memory / 1024 / 1024;
 
-      osStats.push(stat);
-      if (osStats.length >= config.retention) osStats.shift();
+      span.osStats.push(stat);
+      if (span.osStats.length >= span.retention) span.osStats.shift();
+      if (span.responses[0] && span.responses[0].timestamp + (span.interval * span.retention * 1000) < Date.now()) span.responses.shift();
 
-      sendMetrics(io);
+      sendMetrics(io, span);
     });
   };
 
-  const sendMetrics = (io) => {
+  const sendMetrics = (io, span) => {
     io.emit('stats', {
-      osStats,
-      responseTimes
+      osStats: span.osStats,
+      responses: span.responses,
     });
   };
 
@@ -47,13 +49,21 @@
       config.socketPort = defaultConfig.socketPort;
     }
 
-    if (config.interval === undefined || !config instanceof Number) {
-      config.interval = defaultConfig.interval;
+    if (config.spans === undefined || !config instanceof Array) {
+      config.spans = defaultConfig.span;
     }
 
     const io = require('socket.io')(config.socketPort);
 
-    setInterval(() => gatherOsMetrics(io, config), config.interval * 1000);
+    io.on('connection', (socket) => {
+      console.log('User connected! ' + socket);
+    });
+
+    config.spans.forEach((span) => {
+      span.osStats = [];
+      span.responses = [];
+      setInterval(() => gatherOsMetrics(io, span), span.interval * 1000);
+    });
 
     return (req, res, next) => {
       const startTime = process.hrtime();
@@ -61,13 +71,26 @@
         res.sendFile(path.join(__dirname + '/index.html'));
       } else {
         onHeaders(res, () => {
-          var diff = process.hrtime(startTime);
-          var responseTime = diff[0] * 1e3 + diff[1] * 1e-6;
+          const diff = process.hrtime(startTime);
+          const responseTime = diff[0] * 1e3 + diff[1] * 1e-6;
+          const category = Math.floor(res.statusCode / 100);
 
-          responseTimes.push({
-            endpoint: req.path,
-            responseTime,
-            timestamp: Date.now()
+          config.spans.forEach((span) => {
+            if (span.responses[span.responses.length - 1] !== undefined &&
+              span.responses[span.responses.length - 1].timestamp / 1000 + span.interval > Date.now() / 1000) {
+              span.responses[span.responses.length - 1][category]++;
+              span.responses[span.responses.length - 1].count++;
+            } else {
+              span.responses.push({
+                '2': category === 2 ? 1 : 0,
+                '3': category === 3 ? 1 : 0,
+                '4': category === 4 ? 1 : 0,
+                '5': category === 5 ? 1 : 0,
+                count: 1,
+                mean: responseTime,
+                timestamp: Date.now()
+              });
+            }
           });
         });
 
