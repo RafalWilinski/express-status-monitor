dc09f4b15208b7cd495566730fd0f54e93fc16f7
mattia richetto <mrichetto@opentable.com>
2016-08-28 17:37:45 +0100

    use express-favicon-short-circuit

--- examples/index.js
+++ examples/index.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 
 app.use(require('../index')({ path: '/' }));
+app.use(require('express-favicon-short-circuit'));
 
 app.listen(3000, () => {
   console.log('listening on http://0.0.0.0:3000');
