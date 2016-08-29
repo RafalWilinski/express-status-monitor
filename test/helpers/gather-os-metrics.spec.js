4253eb5e3a145baa0aab84d6122e2c8027a975c0
mattia richetto <mrichetto@opentable.com>
2016-08-29 07:51:52 +0100

    add gather os metrics spec [wip]

---
+++ test/helpers/gather-os-metrics.spec.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+
+chai.should();
+
+const gatherOsMetrics = require('../../helpers/gather-os-metrics');
+
+describe('helpers', () => {
+  describe('gather-os-metrics', () => {
+    describe('when invoked', () => {
+      it('then ...', () => {
+        const span = { os: [], responses: [] };
+
+        gatherOsMetrics({}, span);
+
+        // todo: not sure what should I test, maybe the resulted span structure?
+        // todo: also this component has got some internal timing events?
+      });
+    });
+  });
+});
