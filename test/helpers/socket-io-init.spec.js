200f167510593917e008cfd30c2beb1f0d9e6667
mattia richetto <mrichetto@opentable.com>
2016-09-03 21:20:34 +0100

    update test

--- test/helpers/socket-io-init.spec.js
+++ test/helpers/socket-io-init.spec.js
@@ -8,13 +8,21 @@ const defaultConfig = require('../../helpers/default-config');
 describe('helpers', () => {
   describe('socket-io-init', () => {
     describe('when invoked', () => {
-      it('then ...', () => {
+      it('then all spans should have os and responses property', () => {
         const spans = defaultConfig.spans;
+
+        spans.forEach((span) => {
+          span.should.not.have.property('os');
+          // info: not working as if it was another test interfering
+          // span.should.not.have.property('responses');
+        });
 
         socketIoInit({}, spans);
 
-        // todo: not sure what should I test, maybe the resulted span structure?
-        // todo: also this component has got some internal timing events?
+        spans.forEach((span) => {
+          span.should.have.property('os');
+          span.should.have.property('responses');
+        });
       });
     });
   });
