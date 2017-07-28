b1898be41f509cbe4b78bea678f4d1499cdb3220
Rafal Wilinski <raf.wilinski@gmail.com>
2017-07-28 18:08:54 +0200

    Code style fix

--- src/helpers/validate.js
+++ src/helpers/validate.js
@@ -6,15 +6,10 @@ module.exports = config => {
   }
 
   config.title = (typeof config.title === 'string') ? config.title : defaultConfig.title;
-
   config.path = (typeof config.path === 'string') ? config.path : defaultConfig.path;
-
   config.spans = (typeof config.spans === 'object') ? config.spans : defaultConfig.spans;
-
   config.port = (typeof config.port === 'number') ? config.port : defaultConfig.port;
-
   config.websocket = (typeof config.websocket === 'object') ? config.websocket : defaultConfig.websocket;
-
   config.iframe = (typeof config.iframe === 'boolean') ? config.iframe : defaultConfig.iframe;
 
   return config;
