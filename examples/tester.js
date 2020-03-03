40739931053802842ca8a25111628647358f0a89
Rafal Wilinski <raf.wilinski@gmail.com>
2020-03-03 14:00:25 +0100

    0.1.9 - Remove request library, bump dependencies

--- examples/tester.js
+++ examples/tester.js
@@ -1,15 +1,15 @@
-const request = require('request');
+const axios = require("axios");
 
 const port = 3000;
-
-const requestUrl = `http://localhost:${port}/return-status/`;
 const interval = 50;
+const requestUrl = `http://0.0.0.0:${port}/return-status/`;
 
-const makeDummyCall = () => setTimeout(() => {
-  const code = 200 + Math.random() * 399;
-  request.get(`${requestUrl}${code}`);
+const makeDummyCall = () =>
+  setTimeout(() => {
+    const code = 200 + Math.random() * 399;
+    axios.get(`${requestUrl}${code}`).catch(() => {});
 
-  makeDummyCall();
-}, interval);
+    makeDummyCall();
+  }, interval);
 
 makeDummyCall();
