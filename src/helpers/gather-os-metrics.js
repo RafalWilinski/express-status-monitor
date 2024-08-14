c9754799f8f34bf1aa54eb78d5d1227a08774359
intern1-arizon <intern@arizonsystems.com>
2024-08-14 12:20:36 +0530

    logging status in db

--- src/helpers/gather-os-metrics.js
+++ src/helpers/gather-os-metrics.js
@@ -3,16 +3,13 @@ const os = require('os');
 const v8 = require('v8');
 const sendMetrics = require('./send-metrics');
 const debug = require('debug')('express-status-monitor');
+const { PrismaClient } = require('@prisma/client');
 
-let eventLoopStats; // eslint-disable-line
+const prisma = new PrismaClient();
 
-try {
-  eventLoopStats = require('event-loop-stats'); // eslint-disable-line
-} catch (error) {
-  console.warn('event-loop-stats not found, ignoring event loop metrics...');
-}
+let lastDatabaseLog = 0;
 
-module.exports = (io, span) => {
+module.exports = (io, span, config) => {
   const defaultResponse = {
     2: 0,
     3: 0,
@@ -37,16 +34,39 @@ module.exports = (io, span) => {
     stat.timestamp = Date.now();
     stat.heap = v8.getHeapStatistics();
 
-    if (eventLoopStats) {
-      stat.loop = eventLoopStats.sense();
-    }
-
     span.os.push(stat);
     if (!span.responses[0] || (last.timestamp + span.interval) * 1000 < Date.now()) {
       span.responses.push(defaultResponse);
     }
 
-    // todo: I think this check should be moved somewhere else
+    // Database logging
+    if (stat.timestamp - lastDatabaseLog >= config.databaseLoggingInterval * 1000) {
+      lastDatabaseLog = stat.timestamp;
+      prisma.statusLog.create({
+        data: {
+          timestamp: new Date(stat.timestamp),
+          cpuCount: os.cpus().length,
+          memory: stat.memory,
+          pid: stat.pid,
+          ppid: stat.ppid,
+          ctime: BigInt(stat.ctime),
+          elapsed: stat.elapsed,
+          load1: stat.load[0],
+          load5: stat.load[1],
+          load15: stat.load[2],
+          heapTotal: BigInt(stat.heap.total_heap_size),
+          heapUsed: BigInt(stat.heap.used_heap_size),
+          response2xx: last[2],
+          response3xx: last[3],
+          response4xx: last[4],
+          response5xx: last[5],
+          responseMean: last.mean,
+        },
+      }).catch(error => {
+        debug('Error logging to database:', error);
+      });
+    }
+
     if (span.os.length >= span.retention) span.os.shift();
     if (span.responses[0] && span.responses.length > span.retention) span.responses.shift();
 
