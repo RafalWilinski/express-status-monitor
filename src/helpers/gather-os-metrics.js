1804c3a22b959994e0688e390d66e26c513eae58
intern1-arizon <intern@arizonsystems.com>
2024-09-10 19:16:28 +0530

    bug fix

--- src/helpers/gather-os-metrics.js
+++ src/helpers/gather-os-metrics.js
@@ -22,54 +22,63 @@ module.exports = (io, span, config) => {
 
   pidusage(process.pid, (err, stat) => {
     if (err) {
-      debug(err);
+      debug('Error in pidusage:', err);
       return;
     }
 
-    const last = span.responses[span.responses.length - 1];
-
-    // Convert from B to MB
-    stat.memory = stat.memory / 1024 / 1024;
-    stat.load = os.loadavg();
-    stat.timestamp = Date.now();
-    stat.heap = v8.getHeapStatistics();
-
-    span.os.push(stat);
-    if (!span.responses[0] || (last.timestamp + span.interval) * 1000 < Date.now()) {
-      span.responses.push(defaultResponse);
+    if (!stat) {
+      debug('No stat data returned from pidusage');
+      return;
     }
 
-    // Database logging
-    if (stat.timestamp - lastDatabaseLog >= config.databaseLoggingInterval * 1000) {
-      lastDatabaseLog = stat.timestamp;
-      prisma.statusLog.create({
-        data: {
-          timestamp: new Date(stat.timestamp),
-          cpuCount: os.cpus().length,
-          memory: stat.memory,
-          pid: stat.pid,
-          ppid: stat.ppid,
-          ctime: BigInt(stat.ctime),
-          elapsed: stat.elapsed,
-          load1: stat.load[0],
-          load5: stat.load[1],
-          load15: stat.load[2],
-          heapTotal: BigInt(stat.heap.total_heap_size),
-          heapUsed: BigInt(stat.heap.used_heap_size),
-          response2xx: last[2],
-          response3xx: last[3],
-          response4xx: last[4],
-          response5xx: last[5],
-          responseMean: last.mean,
-        },
-      }).catch(error => {
-        debug('Error logging to database:', error);
-      });
+    try {
+      const last = span.responses[span.responses.length - 1] || defaultResponse;
+
+      // Convert from B to MB
+      stat.memory = stat.memory / 1024 / 1024;
+      stat.load = os.loadavg();
+      stat.timestamp = Date.now();
+      stat.heap = v8.getHeapStatistics();
+
+      span.os.push(stat);
+      if (!span.responses[0] || (last.timestamp + span.interval) * 1000 < Date.now()) {
+        span.responses.push(defaultResponse);
+      }
+
+      // Database logging
+      if (stat.timestamp - lastDatabaseLog >= config.databaseLoggingInterval * 1000) {
+        lastDatabaseLog = stat.timestamp;
+        prisma.statusLog.create({
+          data: {
+            timestamp: new Date(stat.timestamp),
+            cpuCount: os.cpus().length,
+            memory: stat.memory,
+            pid: stat.pid,
+            ppid: stat.ppid || 0,
+            ctime: BigInt(stat.ctime || 0),
+            elapsed: stat.elapsed || 0,
+            load1: stat.load[0],
+            load5: stat.load[1],
+            load15: stat.load[2],
+            heapTotal: BigInt(stat.heap.total_heap_size),
+            heapUsed: BigInt(stat.heap.used_heap_size),
+            response2xx: last[2] || 0,
+            response3xx: last[3] || 0,
+            response4xx: last[4] || 0,
+            response5xx: last[5] || 0,
+            responseMean: last.mean || 0,
+          },
+        }).catch(error => {
+          debug('Error logging to database:', error);
+        });
+      }
+
+      if (span.os.length >= span.retention) span.os.shift();
+      if (span.responses[0] && span.responses.length > span.retention) span.responses.shift();
+
+      sendMetrics(io, span);
+    } catch (error) {
+      debug('Error in gather-os-metrics:', error);
     }
-
-    if (span.os.length >= span.retention) span.os.shift();
-    if (span.responses[0] && span.responses.length > span.retention) span.responses.shift();
-
-    sendMetrics(io, span);
   });
 };
